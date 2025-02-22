71c95ac9e2021aea44e3e8f64a7097f5079ab8c4
b22241 <freesoul6800@gmail.com>
2025-02-23 00:48:04 +0530

    fsd

--- controllers/capsuleRoute.js
+++ controllers/capsuleRoute.js
@@ -21,14 +21,23 @@ capsuleRouter.get('/capsules', isLoggedIn, async (req, res) => {
         const lockedCapsules = user.capsules.filter(capsule => capsule.unlockDate > currentDate);
         
         console.log(unlockedCapsules, lockedCapsules);
-        res.render('myCapsules', { unlockedCapsules, lockedCapsules, user });
+        res.render('myCapsules', { unlockedCapsules, lockedCapsules, user,username: req.user.name });
     } catch (error) {
         console.error('Error retrieving capsules:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
 
-capsuleRouter.post('/capsules/create', isLoggedIn, createCapsule);
+capsuleRouter.post('/capsules/create', isLoggedIn, async (req, res) => {
+    try {
+        await createCapsule(req, res);
+        res.redirect('/dashboard'); // Redirect to dashboard after creation
+    } catch (error) {
+        console.error("Error creating capsule:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 
 capsuleRouter.get('/capsule/:capsuleId', isLoggedIn, async (req, res) => {
     try {
@@ -46,7 +55,7 @@ capsuleRouter.get('/capsule/:capsuleId', isLoggedIn, async (req, res) => {
             return res.status(404).json({ message: 'Capsule not found' });
         }
     
-        res.render('individualCapsule',{capsule:capsule});
+        res.render('individualCapsule',{capsule:capsule,username: req.user.name});
     } catch (error) {
         console.error('Error retrieving capsule:', error);
         res.status(500).json({ message: 'Internal server error' });
